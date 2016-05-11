521c277fe2f78467c77524cc03d14126c155d795
ti-shawon <1405050_ti@ugrad.cse.buet.ac.bd>
2016-05-11 09:00:04 +0600

    Add files via upload

---
+++ FactoryDesignPattern-master/EnemyShipTesting.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package factory_design_pattern;
+
+import java.util.Scanner;
+/**
+ *
+ * @author ASUS
+ */
+public class EnemyShipTesting {
+
+    public static void main(String[] args) {
+        
+         // Create the factory object
+	        EnemyShipFactory shipFactory = new EnemyShipFactory();
+	         
+	        // Enemy ship object
+	         
+	        EnemyShip theEnemy = null;
+	         
+	        Scanner userInput = new Scanner(System.in);
+	         
+	        System.out.print("What type of ship? (U / R / B)");
+	         
+	        if (userInput.hasNextLine()){
+	             
+	            String typeOfShip = userInput.nextLine();
+	         
+	            theEnemy = shipFactory.makeEnemyShip(typeOfShip);
+	             
+	            if(theEnemy != null){
+	                 
+	                doStuffEnemy(theEnemy);
+	                 
+	            } else System.out.print("Please enter U, R, or B next time");
+	         
+	        }
+    }  
+
+	     
+    public static void doStuffEnemy(EnemyShip anEnemyShip){
+
+        anEnemyShip.displayEnemyShip();
+
+        anEnemyShip.followHeroShip();
+
+        anEnemyShip.enemyShipShoots();
+
+    }
+        
+}
